816e1fd105879c61c23c007ec77f66770e24e2f6
svetlana <masterica75@mail.ru>
2022-12-08 22:12:16 +0700

    Added Task 1,2,3

---
+++ task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №21. 
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// ● A (7,-5); B (1,-1) -> 7,21
+
+
+Console.Write("Введите координату Хa: ");
+int Xa = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Ya: ");
+int Ya = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Хb: ");
+int Xb = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Yb: ");
+int Yb = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt((Xa - Xb) * (Xa - Xb) + (Ya - Yb) * (Ya - Yb));
+Console.WriteLine(result);
---
+++ task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №22. 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int num = 1;
+
+
+while (num <= N)
+{
+    int result = num * num;
+    Console.Write(result + ",");
+    num++;
+}
+
+
+
